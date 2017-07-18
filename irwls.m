deee28aebd7626e4cd97cf1bf0b703676ae3013c
Gregory Ditzler <ditzler@email.arizona.edu>
2017-07-18 16:46:52 -0700

    updated

--- irwls.m
+++ irwls.m
@@ -1,8 +1,21 @@
 function x = irwls(A, b, max_iter, epsilon)
-% https://web.stanford.edu/~boyd/papers/pdf/rwl1.pdf
+% x = irwls(A, b, max_iter, epsilon)
+% 
+% Reweighted L1 Minimization
+% min |x|_1
+% s.t. Ax=b
+%
+% INPUT 
+%   A: Matrix n x p where p > n
+%   b: Vector n x 1
+%   max_iter: Max number of iterations to run
+%   epsilon: See paper 
+% 
+% Paper link: 
+%   https://web.stanford.edu/~boyd/papers/pdf/rwl1.pdf
 [~, p] = size(A);
 w = ones(p, 1);
-
+w_old = w;
 
 for i = 1:max_iter
   
@@ -16,9 +29,10 @@ for i = 1:max_iter
   cvx_end
   
   w = 1./(epsilon + abs(xhat));
-  
+  if norm(w-w_old, 2) < 1e-6
+    break
+  end
+  w_old = w;
 end
 x = xhat;
 x(abs(x)<1e-3) = 0;
-
-
